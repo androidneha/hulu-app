c0fa054392a7d437eb62500f81b33e7d1409bf48
androidneha <neha.8xx@gmail.com>
2021-05-18 19:38:00 +0530

    complete code

--- README.md
+++ README.md
@@ -18,6 +18,22 @@ You can start editing the page by modifying `pages/index.js`. The page auto-upda
 
 The `pages/api` directory is mapped to `/api/*`. Files in this directory are treated as [API routes](https://nextjs.org/docs/api-routes/introduction) instead of React pages.
 
+
+## HULU-Clone
+
+## Key Points
+* Fully Responsive and Animated Header
+* Fully Responsive and Animated Navbar
+* Server Side Rendering
+* Lazy Loading
+* Smoth UI
+
+
+## Ouput
+
+<img src="https://raw.githubusercontent.com/androidneha/hulu-app/master/output.gif" alt="MERN-Admin-Panel">
+
+
 ## Learn More
 
 To learn more about Next.js, take a look at the following resources:
---
+++ pages/components/Header.js
@@ -0,0 +1,29 @@
+import {
+    BadgeCheckIcon,
+    CollectionIcon,
+    HomeIcon,
+    LightningBoltIcon,
+    SearchIcon,
+    UserIcon
+} from '@heroicons/react/outline'
+import Image from 'next/image'
+import HeaderItem from './HeaderItem'
+
+function Header() {
+    return(
+        <header className="flex flex-col sm:flex-row justify-between items-center h-auto">
+            <div className="flex flex-grow justify-evenly m-5 max-w-2xl">
+                <HeaderItem title='Home' Icon={HomeIcon}/>
+                <HeaderItem title='TRENDING' Icon={LightningBoltIcon}/>
+                <HeaderItem title='VERIFIED' Icon={BadgeCheckIcon}/>
+                <HeaderItem title='COLLECTIONS' Icon={CollectionIcon}/>
+                <HeaderItem title='SEARCH' Icon={SearchIcon}/>
+                <HeaderItem title='Account' Icon={UserIcon}/>
+            </div>
+
+            <Image src='https://links.papareact.com/ua6' width={200} height={100}/>
+        </header>
+    )
+}
+
+export default Header;
